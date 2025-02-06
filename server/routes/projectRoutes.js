4d76febe53d410c2c8144a7c23b2c68597970c30
Henabakos <henogato9876@gmail.com>
2025-02-06 16:07:26 +0300

    added task and project routes

--- server/routes/projectRoutes.js
+++ server/routes/projectRoutes.js
@@ -2,6 +2,8 @@ const express = require("express");
 const {
   createProject,
   getAllProjects,
+  updateProject,
+  deleteProject,
 } = require("../controllers/projectController");
 const { authMiddleware, checkRole } = require("../middleware/auth.middleware");
 
@@ -20,5 +22,19 @@ router.post("/", authMiddleware, checkRole(["admin", "member"]), createProject);
  * @access  Private (Authenticated Users)
  */
 router.get("/", authMiddleware, getAllProjects);
+
+/**
+ * @route PUT /api/projects/:id
+ * @desc Update project details
+ * @access Private (Only Project Admins)
+ */
+router.put("/:id", authMiddleware, checkRole(["Admin"]), updateProject);
+
+/**
+ * @route DELETE /api/projects/:id
+ * @desc Delete a project
+ * @access Private (Only Project Admins)
+ */
+router.delete("/:id", authMiddleware, checkRole(["Admin"]), deleteProject);
 
 export default router;
--- server/routes/taskRoutes.js
+++ server/routes/taskRoutes.js
@@ -1,5 +1,11 @@
 const express = require("express");
-const { createTask, getTasks } = require("../controllers/taskController");
+const {
+  createTask,
+  getTasks,
+  updateTask,
+  deleteTask,
+  getTaskById,
+} = require("../controllers/taskController");
 const { authMiddleware } = require("../middleware/auth.middleware");
 
 const router = express.Router();
@@ -10,6 +16,29 @@ const router = express.Router();
  * @access  Private (Members & Admins)
  */
 router.post("/", authMiddleware, createTask);
+
+/**
+ * @route   PUT /api/tasks/:id
+ * @desc    Update task details
+ * @access  Private (Only Task Owner)
+ */
+
+router.put("/:id", authMiddleware, updateTask);
+
+/**
+ * @route   DELETE /api/tasks/:id
+ * @desc    Delete a task
+ * @access  Private (Only Task Owner)
+ */
+
+router.delete("/:id", authMiddleware, deleteTask);
+
+/**
+ * @route   GET /api/tasks/:id
+ * @desc    Fetch a task by ID
+ * @access  Private (Authenticated Users)
+ */
+router.get("/:id", authMiddleware, getTaskById);
 
 /**
  * @route   GET /api/tasks
